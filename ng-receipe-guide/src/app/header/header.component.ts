a47eb052397181359b81860a41d75c92596f4f04
Margaryta <mvite@softserveinc.com>
2019-07-10 16:08:19 +0300

    Refactor Recipes components; implement base shopping list functionality

--- ng-receipe-guide/src/app/header/header.component.ts
+++ ng-receipe-guide/src/app/header/header.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect (feature: string) {
+    this.featureSelected.emit(feature);
   }
 
 }
